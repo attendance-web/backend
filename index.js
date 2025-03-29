159211777d66d96bdc3f77b2ba19a173be99a37a
serrie <mohammadzidane058@gmail.com>
2025-03-29 18:39:18 +0700

    tes

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.use('/user', userRoutes);
 app.use('/logs', logsRoutes);
 
 const PORT = 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
-// module.exports = app;
+module.exports = app;
